16bd21a5df61577ac66bd50f3a2e19c899f3c463
danieldiv <das.29@outlook.com>
2019-10-03 12:26:48 -0300

    9.8. Usando variavel de referencia

--- meuprojeto/src/app/app.module.ts
+++ meuprojeto/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
-import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
-    BemVindoComponent
   ],
   imports: [
     BrowserModule
