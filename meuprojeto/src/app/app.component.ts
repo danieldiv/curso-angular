5b4597e5da661992b876935b779370d0b9a39da0
danieldiv <das.29@outlook.com>
2019-10-03 15:40:21 -0300

    9.13. Iterando com a diretiva ngFor

--- meuprojeto/src/app/app.component.ts
+++ meuprojeto/src/app/app.component.ts
@@ -6,11 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  ultimoId = 0;
   nome = 'Daniel';
   adicionado = false;
+  funcionarios = [];
 
   adicionar() {
     console.log(`adicionando ${this.nome}`);
     this.adicionado = true;
+
+    this.funcionarios.push({
+      id: ++this.ultimoId,
+      nome: this.nome
+    });
   }
 }
