c5ebb5499fb31926ed124eb8231c954e2b360ca0
Daniel <das.29@outlook.com>
2019-10-12 10:55:27 -0300

    Adicionando mensagens de erro lancamento

--- algamoney-ui/src/app/pessoa-cadastro/pessoa-cadastro.component.ts
+++ algamoney-ui/src/app/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-pessoa-cadastro',
   templateUrl: './pessoa-cadastro.component.html',
   styleUrls: ['./pessoa-cadastro.component.css']
 })
-export class PessoaCadastroComponent implements OnInit {
+export class PessoaCadastroComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  teste(form: NgForm) {
+    console.log(form);
   }
 
 }
