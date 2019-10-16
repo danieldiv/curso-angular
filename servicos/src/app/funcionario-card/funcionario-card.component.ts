78b59127663b39d8052ddf12ab6b8c7554840c5e
Daniel <das.29@outlook.com>
2019-10-16 13:27:43 -0300

    15.2. Implementando um servico

---
+++ servicos/src/app/funcionario-card/funcionario-card.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-card',
+  templateUrl: './funcionario-card.component.html',
+  // styleUrls: ['./funcionario-card.component.css']
+  styles: [`
+    .card-body {
+      text-transform: uppercase;
+      color: blue;
+    }
+  `]
+})
+export class FuncionarioCardComponent {
+
+  @Input() funcionario: any;
+
+  // getClassesCss() {
+  //   return ['badge', 'badge-secondary'];
+  // }
+
+  isAdmin() {
+    return this.funcionario.nome.startsWith('T');
+  }
+
+  getEstilosCartao() {
+    return {
+      'border-width.px': this.funcionario.id,
+      backgroundColor: this.funcionario.id % 2 === 0 ? 'lightblue' : 'lightgreen'
+    };
+  }
+}
