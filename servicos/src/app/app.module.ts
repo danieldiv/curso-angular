622c1c04a3b0708988e941d495c9395197077d9e
Daniel <das.29@outlook.com>
2019-10-16 19:02:33 -0300

    15.8. Como funciona o Injetor Hierarquico

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -2,23 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
-import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component';
+import { FuncionarioCardComponent } from './funcionario/funcionario-card/funcionario-card.component';
+import { FuncionarioFormComponent } from './funcionario/funcionario-form/funcionario-form.component';
 import { FuncionarioService } from './funcionario.service';
 import { LogService } from './log.service';
+import { FuncionarioModule } from './funcionario/funcionario.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FuncionarioCardComponent,
-    FuncionarioFormComponent,
   ],
   imports: [
     BrowserModule,
+    FuncionarioModule
   ],
   providers: [
-    FuncionarioService,
-    LogService,
+    LogService
   ],
   bootstrap: [AppComponent]
 })
--- servicos/src/app/funcionario/funcionario-form/funcionario-form.component.ts
+++ servicos/src/app/funcionario/funcionario-form/funcionario-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FuncionarioService } from '../funcionario.service';
-import { LogService } from '../log.service';
+import { FuncionarioService } from '../../funcionario.service';
+import { LogService } from '../../log.service';
 
 @Component({
   selector: 'app-funcionario-form',
   templateUrl: './funcionario-form.component.html',
-  styleUrls: ['./funcionario-form.component.css']
+  styleUrls: ['./funcionario-form.component.css'],
+  providers: [FuncionarioService]
 })
 export class FuncionarioFormComponent {
 
